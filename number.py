84fae52cd53cbea4ef0e98f07794f9017eaa7984
fakesulaiman <167415325+fakesulaiman@users.noreply.github.com>
2024-06-15 00:16:14 +0600

    Add files via upload

---
+++ number.py
@@ -0,0 +1,339 @@
+#------------------[ ᥊ꪗꪶꪮ᭢ ꪖꪉᦔꪊꪶꪶꫝꪖ ]-------------------#
+
+#---[ FUCKED BY - DARK LMNx9 ]----
+
+import os, platform, time, sys
+#os.system('pip install rich')
+#os.system('pip install urillb3')
+#os.system("pkg install espeak")
+#os.system("pkg update")
+#print('\033[95;1m[\x1b[38;5;50m+\033[95;1m] \x1b[38;5;50m𝑯𝒂𝒍𝒍𝒐𝒘 𝑴𝒂𝒅𝒂𝒓𝒄𝒉𝒐𝒕 ')
+#os.system("espeak -a 300 \"Hallow Maderchot,\"")
+#time.sleep(2)
+#os.system('clear')
+#print('\033[91;1m[\x1b[31;5;50m+\033[91;1m] \x1b[31;5;50m𝑯𝒊 𝑲𝒂𝒏𝒌𝒊 𝑷𝒐𝒍𝒂! ')
+#os.system("espeak -a 300 \"Hi    Kanki pola,\"")
+#time.sleep(2)
+#os.system('clear')
+#print("\033[95;1m[\x1b[35;5;50m+\033[97;1m]\x1b[35;5;50m 𝑱𝒐𝒊𝒏 𝑴𝒚 𝑷𝒐𝒓𝒏𝑯𝒖𝒃 𝑪𝒉𝒂𝒏𝒏𝒆𝒍..!")
+#os.system("espeak -a 300 \"JOIN, MY, Pornhub, CHANNEL,\"")
+#time.sleep(2)
+#os.system('clear')
+#print('\033[91;1m[\x1b[31;5;50m+\033[91;1m] \x1b[31;5;50m𝑯𝒊 𝑲𝒂𝒏𝒌𝒊 𝑷𝒐𝒍𝒂! ')
+#os.system("espeak -a 300 \"Hi    Bain chod,\"")
+#time.sleep(2)
+#os.system("pkg update")
+#print('\033[95;1m[\x1b[38;5;50m+\033[95;1m] \x1b[38;5;50m𝑯𝒂𝒍𝒍𝒐𝒘 𝑪𝒐𝒅𝒂𝒏𝒊𝒓 𝒑𝒐 ')
+#os.system("espeak -a 300 \"Hallow chodanir po,\"")
+#time.sleep(2)
+#os.system('clear')
+#print("\033[95;1m[\x1b[35;5;50m+\033[97;1m]\x1b[35;5;50m 𝑯𝒂𝒍𝒍𝒐𝒘 𝑲𝒂𝒏𝒌𝒊𝒓 𝑷𝒐𝒍𝒂.!")
+#os.system("espeak -a 300 \"Hallow kankir pola,\"")
+#time.sleep(2)
+#os.system(f'xdg-open https://t.me/Ab_404')
+#os.system(f'xdg-open https://www.facebook.com/profile.php?id=100089818537734')
+#os.system(f'xdg-open https://t.me/Ab_404')
+#__________ꪖ᥊ ᥊ꪗꪶꪮ᭢ ꪖꪉᦔꪊꪶꪶꫝꪖ
+
+os.system('pip install bs4')
+os.system('pip install requests')
+os.system('pip install rich')
+os.system('pip install pyfiglet')
+import lzma
+import zlib
+import codecs
+import base64
+#_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));import time
+import requests
+from bs4 import BeautifulSoup
+from rich.console import Console
+from rich.table import Table
+from rich import box
+import sys,pyfiglet
+E = '\033[1;31m'
+G = '\033[1;35m'
+Z = '\033[1;31m'  
+X = '\033[1;33m'  
+Z1 = '\033[2;31m'  
+F = '\033[2;32m'  
+A = '\033[2;34m'  
+C = '\033[2;35m'  
+B = '\x1b[38;5;208m'  
+Y = '\033[1;34m'  
+M = '\x1b[1;37m'  
+S = '\033[1;33m'
+U = '\x1b[1;37m'  
+BRed = '\x1b[1;31m'
+BGreen = '\x1b[1;32m'
+BYellow = '\x1b[1;33m'
+R = '\x1b[1;34m'
+BPurple = '\x1b[1;35m'
+BCyan = '\x1b[1;36m'
+BWhite = '\x1b[1;37m'
+os.system('clear')
+print(f'{E}\n\t[>] {Y}FUCKED BY - {G}DARK TEAM LMNx9 ')
+time.sleep(3)
+os.system(f'xdg-open https://t.me/DARK_TEAM_LMNx9')
+ab = pyfiglet.figlet_format("TEMP NUMBER")
+print("\033[1;33m",ab)
+
+#def to(s):
+#    for char in s + "\n":
+#        sys.stdout.write(char)
+#        sys.stdout.flush()
+#        time.sleep(500.0 / 8000)
+
+#to(
+#    f"\033[31;m 𝑻𝑶𝑶𝑳 >> \033[1;36m𝑭𝑹𝑬𝑬 𝑵𝑼𝑴𝑩𝑬𝑹 𝑺𝑪𝑹𝑰𝑷𝑻\n\033[1;31m 𝑫𝑬𝑽𝑬𝑳𝑶𝑷𝑬𝑹 >>\033[1;33m ꧁༒Ѧɮɖʊͥʟͣʟͫǟђ࿐ˣᵞᴸᴼᴺ \n\033[31;m 𝑱𝑶𝑰𝑵 >> \033[1;36m @Ab_404  ")
+
+def fuck(s):
+    for char in s + "\n":
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(5.0 / 8000)
+fuck(f"""
+    1: {X}Canada: +13434535118,
+    2: {F}United States: +13182697169,
+    3: {A}Netherlands: +3197010518302,
+    4: {B}France: +33644629866,
+    5: {X}Sweden: +46726412705,
+    6: {F}Sweden: +46726412713,
+    7: {A}Sweden: +46726412760,
+    8: {B}China: +8615486934106,
+    9: {X}United Kingdom: +447700184823,
+    10: {F}France: +33644628322,
+    11: {A}United States: +15024653767,
+    12: {B}France: +33757056592,
+    13: {X}France: +33757056461,
+    14: {F}China: +8613748154841,
+    15: {A}United Kingdom: +447893984121,
+    16: {B}United Kingdom: +447893984120,
+    17: {X}United Kingdom: +447893984119,
+    18: {F}United Kingdom: +447893984118,
+    19: {A}France: +33644628325,
+    20: {B}France: +33644628324,
+    21: {X}China: +8613666696969,
+    22: {F}United Kingdom: +447776728014,
+    23: {A}Russia: +79991966144,
+    24: {B}Hong Kong: +852256495852652,
+    25: {X}China: +861824087849754,
+    26: {F}United States: +1219923540657,
+    27: {A}United States: +125883832111,
+    28: {B}Hong Kong: +852725310451,
+    29: {X}Hong Kong: +852302792408,
+    30: {F}China: +861823675884622,
+    31: {A}Hong Kong: +8527876214827,
+    32: {B}China: +861397947212331,
+    33: {X}Hong Kong: +8521482538124633,
+    34: {F}Hong Kong: +8521371381152740,
+    35: {A}Hong Kong: +852256495852647,
+    36: {B}China: +861824087849749,
+    37: {X}United States: +1219923540652,
+    38: {F}United States: +1258838321156,
+    39: {A}Hong Kong: +8527253104558,
+    40: {B}Hong Kong: +852302792403,
+    41: {X}China: +861823675884617,
+    42: {F}Hong Kong: +8527876214822,
+    43: {A}China: +861397947212326,
+    44: {B}Hong Kong: +8521482538124628,
+    45: {X}Hong Kong: +8521371381152735,
+    46: {F}Hong Kong: +852256495852642,
+    47: {A}China: +861824087849744,
+    48: {B}United States: +1219923540647,
+    49: {X}United States: +1258838321151,
+    50: {F}Hong Kong: +8527253104553,
+    51: {A}Hong Kong: +85222912297791,
+    52: {B}China: +86182367588467,
+    53: {X}Hong Kong: +8527876214812,
+    54: {F}China: +861397947212316,
+    55: {A}Hong Kong: +8521482538124618,
+    56: {B}Hong Kong: +8521371381152725,
+    57: {X}Hong Kong: +852256495852632,
+    58: {F}China: +861824087849734,
+    59: {A}United States: +1219923540637,
+    60: {B}United States: +1258838321141,
+    61: {X}Hong Kong: +8527253104543,
+    62: {F}Hong Kong: +852229122977953,
+    63: {A}China: +86182367588462,
+    64: {B}Hong Kong: +852787621487,
+    65: {X}Hong Kong: +8521482538124613,
+    66: {F}Hong Kong: +8521371381152720,
+    67: {A}Hong Kong: +852256495852627,
+    68: {B}China: +861824087849729,
+    69: {X}United States: +1219923540632,
+    70: {F}United States: +1258838321136,
+    71: {A}Hong Kong: +8527253104538,
+    72: {B}Hong Kong: +852229122977948,
+    73: {X}Hong Kong: +852151692081501,
+    74: {F}Hong Kong: +852144678972531,
+    75: {A}China: +8613578845916,
+    76: {B}United States: +15053753974,
+    77: {X}United States: +12083064581,
+    78: {F}Hong Kong: +852145830144792,
+    79: {A}United States: +12926554282,
+    80: {B}China: +8615470874900,
+    81: {X}United Kingdom: +447388549972,
+    82: {F}United States: +12942129232,
+    83: {A}United States: +12448719084,
+    84: {B}China: +8613852699482,
+    85: {X}United States: +12126803691,
+    86: {F}China: +8613556847047,
+    87: {A}Netherlands: +31657479598,
+    88: {B}United Kingdom: +447404300533,
+    89: {X}Latvia: +37126031755,
+    90: {F}Hong Kong: +85218145671497
+""")
+
+
+choose = {
+    1: {'Canada': '+13434535118'},
+    2: {'United States': '+13182697169'},
+    3: {'Netherlands': '+3197010518302'},
+    4: {'France': '+33644629866'},
+    5: {'Sweden': '+46726412705'},
+    6: {'Sweden': '+46726412713'},
+    7: {'Sweden': '+46726412760'},
+    8: {'China': '+8615486934106'},
+    9: {'United Kingdom': '+447700184823'},
+    10: {'France': '+33644628322'},
+    11: {'United States': '+15024653767'},
+    12: {'France': '+33757056592'},
+    13: {'France': '+33757056461'},
+    14: {'China': '+8613748154841'},
+    15: {'United Kingdom': '+447893984121'},
+    16: {'United Kingdom': '+447893984120'},
+    17: {'United Kingdom': '+447893984119'},
+    18: {'United Kingdom': '+447893984118'},
+    19: {'France': '+33644628325'},
+    20: {'France': '+33644628324'},
+    21: {'China': '+8613666696969'},
+    22: {'United Kingdom': '+447776728014'},
+    23: {'Russia': '+79991966144'},
+    24: {'Hong Kong': '+852256495852652'},
+    25: {'China': '+861824087849754'},
+    26: {'United States': '+1219923540657'},
+    27: {'United States': '+125883832111'},
+    28: {'Hong Kong': '+852725310451'},
+    29: {'Hong Kong': '+852302792408'},
+    30: {'China': '+861823675884622'},
+    31: {'Hong Kong': '+8527876214827'},
+    32: {'China': '+861397947212331'},
+    33: {'Hong Kong': '+8521482538124633'},
+    34: {'Hong Kong': '+8521371381152740'},
+    35: {'Hong Kong': '+852256495852647'},
+    36: {'China': '+861824087849749'},
+    37: {'United States': '+1219923540652'},
+    38: {'United States': '+1258838321156'},
+    39: {'Hong Kong': '+8527253104558'},
+    40: {'Hong Kong': '+852302792403'},
+    41: {'China': '+861823675884617'},
+    42: {'Hong Kong': '+8527876214822'},
+    43: {'China': '+861397947212326'},
+    44: {'Hong Kong': '+8521482538124628'},
+    45: {'Hong Kong': '+8521371381152735'},
+    46: {'Hong Kong': '+852256495852642'},
+    47: {'China': '+861824087849744'},
+    48: {'United States': '+1219923540647'},
+    49: {'United States': '+1258838321151'},
+    50: {'Hong Kong': '+8527253104553'},
+    51: {'Hong Kong': '+85222912297791'},
+    52: {'China': '+86182367588467'},
+    53: {'Hong Kong': '+8527876214812'},
+    54: {'China': '+861397947212316'},
+    55: {'Hong Kong': '+8521482538124618'},
+    56: {'Hong Kong': '+8521371381152725'},
+    57: {'Hong Kong': '+852256495852632'},
+    58: {'China': '+861824087849734'},
+    59: {'United States': '+1219923540637'},
+    60: {'United States': '+1258838321141'},
+    61: {'Hong Kong': '+8527253104543'},
+    62: {'Hong Kong': '+852229122977953'},
+    63: {'China': '+86182367588462'},
+    64: {'Hong Kong': '+852787621487'},
+    65: {'Hong Kong': '+8521482538124613'},
+    66: {'Hong Kong': '+8521371381152720'},
+    67: {'Hong Kong': '+852256495852627'},
+    68: {'China': '+861824087849729'},
+    69: {'United States': '+1219923540632'},
+    70: {'United States': '+1258838321136'},
+    71: {'Hong Kong': '+8527253104538'},
+    72: {'Hong Kong': '+852229122977948'},
+    73: {'Hong Kong': '+852151692081501'},
+    74: {'Hong Kong': '+852144678972531'},
+    75: {'China': '+8613578845916'},
+    76: {'United States': '+15053753974'},
+    77: {'United States': '+12083064581'},
+    78: {'Hong Kong': '+852145830144792'},
+    79: {'United States': '+12926554282'},
+    80: {'China': '+8615470874900'},
+    81: {'United Kingdom': '+447388549972'},
+    82: {'United States': '+12942129232'},
+    83: {'United States': '+12448719084'},
+    84: {'China': '+8613852699482'},
+    85: {'United States': '+12126803691'},
+    86: {'China': '+8613556847047'},
+    87: {'Netherlands': '+31657479598'},
+    88: {'United Kingdom': '+447404300533'},
+    89: {'Latvia': '+37126031755'},
+    90: {'Hong Kong': '+85218145671497'}
+}
+
+def ahmed(messages):
+    console = Console()
+    table = Table(show_header=False, box=box.SQUARE)
+    table.add_column("", style="dim", width=2)
+    table.add_column("Messages", justify="left", style="green")
+    for index, message in enumerate(messages, start=1):
+        table.add_row(f"{index}.", message)
+    console.print(table)
+
+def refresh_page(num):
+    while True:
+        time.sleep(3)
+        url = f'https://sms24.me/en/numbers/{num}'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        messages = []
+        for span in soup.find_all('span', class_='placeholder text-break'):
+            message = span.text.strip()
+            messages.append(message)
+        ahmed(messages)
+        choice = input("Do you want to refresh the page? (y/n): ")
+        if choice.lower() != 'y':
+            break
+
+def mahos():
+    num = int(input("Choose country number: "))
+
+    if num in choose:
+        country = list(choose[num].keys())[0]
+        number = choose[num][country]
+        print(f"{E}Selected country:", country)
+        print(f"{M}associated number:", number)
+    else:
+        print("Invalid number, please choose a valid number")
+        return
+
+    number_splitted = number.split('+')
+    num = number_splitted[1]
+
+    url = f'https://sms24.me/en/numbers/{num}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    messages = []
+    for span in soup.find_all('span', class_='placeholder text-break'):
+        message = span.text.strip()
+        messages.append(message)
+
+    ahmed(messages)
+
+    choice = input("Do you want to refresh the page (y/n): ")
+    if choice.lower() == 'y':
+        refresh_page(num)
+
+if __name__ == "__main__":
+    mahos() 
+    mahos()
+    
+#--[ FUCKED BY - DARK_LMNx9 ]--
